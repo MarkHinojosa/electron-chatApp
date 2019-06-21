aec9d6cb9be299bd7e74936fa537e5c05568986e
mark <markhinojosa1212@gmail.com>
2019-06-20 22:54:31 -0500

    Electron running, Router connected, talking with Pusher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Login from './screens/login';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Electron Desktop Starter Template</h1>
+        <Login/>
       </div>
     )
   }
